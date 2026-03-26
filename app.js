8090b57f82d47c18d5b5e7499c1380f2dcc14774
elkas88 <3511783+elkas88@users.noreply.github.com>
2026-03-26 15:28:13 +0100

    some updates

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path         = require('path');
 var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
+const helmet     = require('helmet');
 var moment       = require('moment');
 const createSessionMiddleware = require('./lib/middleware/withSession');
 
